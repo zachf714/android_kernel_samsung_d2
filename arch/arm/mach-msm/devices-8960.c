1f21876e3f10f18e8a00f33b26d8b7e6d788f637
Blackburn <blackburn2929@gmail.com>
2014-03-24 20:03:57 -0500

    Add 500MHz GPU Step

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -3637,6 +3637,11 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	 	.pwrlevel = {
                 {
+                        .gpu_freq = 500000000,
+                        .bus_freq = 6,
+                        .io_fraction = 0,
+                },
+                {
                         .gpu_freq = 480000000,
                         .bus_freq = 5,
                         .io_fraction = 0,
@@ -3667,7 +3672,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
   		},
   	},
 	.init_level = 2,
-	.num_levels = 6,
+	.num_levels = 7,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
